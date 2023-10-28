9c6eaf6bad912579c7182f6ebe208e5625a80787
SoumyaDeori <131858497+SoumyaDeori@users.noreply.github.com>
2023-10-28 22:11:23 +0530

    Update 2 Sum Problem.cpp

--- Medium/2 Sum Problem.cpp
+++ Medium/2 Sum Problem.cpp
@@ -1,1 +1,15 @@
-Hello
+string read(int n, vector<int> arr, int target)
+{
+    // Write your code here.
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]+arr[j]==target)
+            {
+                return "YES";
+            }
+        }
+    }
+    return "NO";
+}
