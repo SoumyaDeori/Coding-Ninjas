619d6b0b9815a012b0382d4c9e1c862079673d93
SoumyaDeori <131858497+SoumyaDeori@users.noreply.github.com>
2023-10-29 20:43:58 +0530

    Update Find Majority Element that occur more than (N\2) times.cpp

--- Medium/Find Majority Element that occur more than (N\2) times.cpp
+++ Medium/Find Majority Element that occur more than (N\2) times.cpp
@@ -1,1 +1,22 @@
-
+int majorityElement(vector<int> v) {
+	// Write your code here
+	int majority=v[0];
+	int count=1;
+	for(int i=1;i<v.size();i++)
+	{
+		if(count==0)
+		{
+			majority=v[i];
+			count = 1;
+		}
+		else if(majority==v[i])
+		{
+			count++;
+		}
+		else
+		{
+			count--;
+		}
+	}
+	return majority;
+}
