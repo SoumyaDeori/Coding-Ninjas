e473834b3d2427c6803bb16ae0fb4af7674fc29c
SoumyaDeori <131858497+SoumyaDeori@users.noreply.github.com>
2023-10-30 23:13:53 +0530

    Update Stock Buy & Sell.cpp

--- Medium/Stock Buy & Sell.cpp
+++ Medium/Stock Buy & Sell.cpp
@@ -1,1 +1,21 @@
-
+int bestTimeToBuyAndSellStock(vector<int>&prices) {
+    // Write your code here.
+    int n = prices.size();
+    if(n<=1){
+        return 0;
+    }
+    int minPrice = prices[0];
+    int maxProfit=0;
+    for(int i=1;i<n;i++)
+    {
+        if(prices[i]<minPrice)
+        {
+            minPrice=prices[i];
+        }
+        else if(prices[i]-minPrice>maxProfit)
+        {
+            maxProfit=prices[i]-minPrice;
+        }
+    }
+    return maxProfit;   
+}
