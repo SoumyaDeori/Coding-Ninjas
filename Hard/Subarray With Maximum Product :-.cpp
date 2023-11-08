0a3dd40faa90fb9d1082e59e34961d9c1f57a17a
SoumyaDeori <131858497+SoumyaDeori@users.noreply.github.com>
2023-11-08 23:56:27 +0530

    Create Subarray With Maximum Product :-.cpp

---
+++ Hard/Subarray With Maximum Product :-.cpp
@@ -0,0 +1,13 @@
+int subarrayWithMaxProduct(vector<int> &arr)
+{
+	// Write your code here.
+     int prod1 = arr[0],prod2 = arr[0],result = arr[0];
+    
+     for(int i=1;i<arr.size();i++) 
+	 {
+        int temp = max({arr[i],prod1*arr[i],prod2*arr[i]});
+        prod2 = min({arr[i],prod1*arr[i],prod2*arr[i]});
+        prod1 = temp;
+	 }    
+    result = max(result,prod1);
+}
